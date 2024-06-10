0e48f57759264af88043ac5c57c9d2e5b530f0b2
AnnLoseva <ann.loseva03@gmail.com>
2024-06-10 20:47:57 +0300

    Homework 8

--- Homework 26.05.2024/Assets/8 Homework/Movement.cs
+++ Homework 26.05.2024/Assets/8 Homework/Movement.cs
@@ -9,42 +9,59 @@ public class Movement : MonoBehaviour
 {
     public GameObject[] objects;
     public Transform[] points;
-    public bool isRandom;
     public float speed = 5;
 
     private Transform target;
-    private int i;
+    private int npcCounter;
+    private int targetCounter;
     private Random random;
-     
-    
+    private bool isBackward;
+    private bool shouldSwitch;
+    private bool canSwitch;
+
+
+    public int currentMovementType = 1;
+    private int nextMovementType = 1;
+
+
 
     private void Start()
     {
 
         random = new Random();
-        i = 0;
-        target = points[i+1];
+        npcCounter = 0;
+        targetCounter = 1;
+        target = points[targetCounter];
 
-        transform.LookAt(target.transform);
-
+        objects[npcCounter].transform.LookAt(target.transform);
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        objects[i].transform.position = Vector3.MoveTowards(objects[i].transform.position, target.position, speed * Time.deltaTime);
+        objects[npcCounter].transform.position = Vector3.MoveTowards(objects[npcCounter].transform.position, target.position, speed * Time.deltaTime);
 
-        if (objects[i].transform.position == target.position)
+        if (objects[npcCounter].transform.position == target.position)
         {
-            if(isRandom)
-            {
-                RandomMovement();
-            }
-            else
+            if (currentMovementType == 1)
             {
                 NormalMovement();
             }
+            else if(currentMovementType == 2)
+            {
+                ForwardBackwardMovement();
+            }
+            else
+            {
+                RandomMovement();
+            }
+        }
+
+        if(canSwitch && shouldSwitch)
+        {
+            currentMovementType = nextMovementType;
+            shouldSwitch = false;
         }
 
 
@@ -52,29 +69,111 @@ public class Movement : MonoBehaviour
 
     private void NormalMovement()
     {
-        
-            if (i < points.Length - 1)
-            {
-                i++;
-            target = points[i + 1];
-        }
-            else if (i>= points.Length - 1)
-            {
-                i = 0;
-            target = points[i+1];
-        }
-            transform.LookAt(target.transform);
-        
 
+        if (targetCounter < points.Length - 1)
+        {
+            npcCounter++;
+            targetCounter++;
+            canSwitch = false;
+        }
+        else 
+        {
+            canSwitch = true;
+            Replace();
+            npcCounter = 0;
+            targetCounter = 0;
+
+        }
+
+        target = points[targetCounter];
+        objects[npcCounter].transform.LookAt(target.transform);
+
+
+    }
+
+    private void ForwardBackwardMovement()
+    {
+        canSwitch = false;
+
+        if (!isBackward)
+        {
+            if (targetCounter < points.Length)
+            {
+                npcCounter++;
+                targetCounter++;
+            }
+            else
+            {
+                canSwitch = true;
+                isBackward = true;
+                targetCounter--;
+            }
+        }
+        else
+        {
+            if (targetCounter > 0)
+            {
+                npcCounter--;
+                targetCounter--;
+            }
+            else
+            {
+                canSwitch = true;
+                isBackward = false;
+
+                npcCounter = 0;
+                targetCounter = 1;
+            }
+        }
+
+        target = points[targetCounter];
+        objects[npcCounter].transform.LookAt(target.transform);
     }
 
     private void RandomMovement()
     {
-            
-            i = random.Next(0, points.Length);
-            target = points[i];
-            transform.LookAt(target.transform);
-        
+        GameObject tempObject = objects[npcCounter];
+        objects[npcCounter] = objects[targetCounter];
+        objects[targetCounter] = tempObject;
+        int rnd = random.Next(0, points.Length);
+        targetCounter = rnd;
+        target = points[targetCounter];
+        objects[npcCounter].transform.LookAt(target.transform);
+
+    }
+
+    private void Replace()
+    {
+        GameObject lastObject = objects[objects.Length-1];
+
+        for (int i = objects.Length - 1; i > 0 ; i--)
+        {
+            objects[i] = objects[i - 1];
+        }
+
+        objects[0] = lastObject;
+    }
+
+    public void NormalMovementButton()
+    {
+        nextMovementType = 1;
+
+        shouldSwitch = true;
+    }
+
+    public void ForwardBackwardMovementButton()
+    {
+        nextMovementType = 2;
+
+        shouldSwitch = true;
+    }
+
+    public void RandomMovementButton()
+    {
+        nextMovementType = 3;
+
+        shouldSwitch = true;
     }
 }
+
 
