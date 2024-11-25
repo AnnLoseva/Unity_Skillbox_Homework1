92dbc27dea50cc53d18c777bdcce86215a0f5c84
AnnLoseva <ann.loseva03@gmail.com>
2024-11-25 10:47:47 +0200

    Zip

--- Skillbox_Platformer/Assets/Scripts/GlobalStringVar.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class GlobalStringVar : MonoBehaviour
-{
-    public const string HORIZONTAL_AXIS = "Horizontal";
-    public const string VERTICAL_AXIS = "Vertical";
-    public const string JUMP = "Jump";
-    public const string ATTACK = "Fire1";
-}
